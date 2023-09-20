8b6828d55edc3f91c3f4ad42f07b0bd9991028d2
Nicholas Lee <nicholas.lee@sagebase.org>
2023-09-20 10:42:00 -0700

    updated data model and folder structure

--- dev/utils/convert_csv.py
+++
@@ -1,8 +0,0 @@
-import yaml
-import pathlib
-
-with open("./local_configs/notebook_config.yaml", "r") as f:
-    config = yaml.safe_load(f)
-
-csv_model = pathlib.Path(config["file_names"]["csv_model"]).resolve()
-json_model = pathlib.Path(config["file_names"]["json_model"]).resolve()
