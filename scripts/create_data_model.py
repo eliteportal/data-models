4b6e052e54a789ba47386a42f3ddff956359ced1
Nicholas Lee <nicholas.lee@sagebase.org>
2024-04-09 18:34:59 -0700

    updated repo structure and workflows

--- scripts/create_data_model.py
+++ scripts/create_data_model.py
@@ -1,20 +1,48 @@
+#!usr/bin python
+
 import os
 from pathlib import Path
 from dotenv import dotenv_values
+import re
 
 # paths to import files
-root_dir = Path(__file__).parent.parent
+# Find ELITE-data-models root directory from anywhere
+ROOT_DIR_NAME = "ELITE-data-models"
+
+for p in Path(__file__).parents:
+    if bool(re.search(ROOT_DIR_NAME + "$", str(p))):
+        print(p)
+        root_dir = p
+
+print(">>> Changing root path to: ", root_dir)
+os.chdir(root_dir)
 
 config = dotenv_values(Path(root_dir, ".env"))
-schematic_config = Path(root_dir, config["schematic_config_path"])
+schematic_config = str(Path(root_dir, config["schematic_config_path"]).resolve())
 csv_model = Path(root_dir, config["csv_path"])
 json_model = Path(root_dir, csv_model.stem + ".jsonld")
 
-# Initialize schematic
-os.system(f"schematic init --config {config['paths']['schematic']}")
+manifests_to_generate = ["Biospecimenhuman"]
 
-# Convert Schema
-os.system(f"schematic schema convert {csv_model} --output_json {json_model}")
+# Initialize schematic
+# print(">>> Initializing Schematic")
+# os.system(f"schematic init --config {schematic_config.resolve()}")
+
+# Convert Schema if schema differs (todo)
+# print(">>> Converting CSV to JSONLD")
+# os.system(f"schematic schema convert {csv_model} --output_jsonld {json_model}")
 
 # Get an empty manifest as a CSV using model
-os.system(f"schematic manifest --config {schematic_config} get")
+# &> tests/logs/manifest_generation.txt
+
+print(">>> Creating manifest template(s)")
+if isinstance(manifests_to_generate, list):
+    for m in manifests_to_generate:
+        print("Creating manifest for: ", m)
+        os.system(
+            f"schematic manifest --config {schematic_config} get -dt {m} --output_xlsx ./tests/manifest-templates/EL.manifest.{m}.xlsx "
+        )
+else:
+    os.system(
+        f"schematic manifest --config {schematic_config} get -dt {manifests_to_generate} --output_xlsx ./tests/manifest-templates/EL.manifest.{manifests_to_generate}.xlsx "
+    )
