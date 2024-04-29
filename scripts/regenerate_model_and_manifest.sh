e694ec65431ab99a297ef36c334ab647a0bdcfe5
Nicholas Lee <nicholas.lee@sagebase.org>
2024-04-29 14:01:46 -0700

    updated descriptions of attributes

--- scripts/regenerate_model_and_manifest.sh
+++ scripts/regenerate_model_and_manifest.sh
@@ -9,17 +9,17 @@ schematic schema convert ./EL.data.model.csv \
   --output_jsonld ./EL.data.model.jsonld
 date "+%H:%M:%S   %d/%m/%y"
 
-for MANIFEST in 'Biospecimenhuman'; do
+for MANIFEST in 'Proteomics'; do
   echo -- $MANIFEST
 
-  RESULTS=$(schematic manifest --config ./config.yml \
-    get -dt $MANIFEST \
-    --output_xlsx EL.Manifest.$MANIFEST.xlsx \
-    --title EL.Manifest.$MANIFEST \
-    --sheet_url)
+  RESULTS=$(
+    schematic manifest --config ./config.yml \
+      get -dt $MANIFEST \
+      -s -oxlsx EL.Manifest.$MANIFEST.xlsx
+  )
 
   echo $RESULTS
   NOW=$(date -f "%d/%m/%y +%H:%M:%S")
-  echo $NOW $MANIFEST ": " $RESULTS >>./_data/manifest_generation_results.txt
+  echo $NOW $MANIFEST ": " $RESULTS >>./data/manifest_generation_results.txt
   echo -----------------------------
 done
