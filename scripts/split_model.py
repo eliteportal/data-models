bd5c221cd7405b99ac45c46182a9340f1f54b3d3
Nicholas Lee <nicholas.lee@sagebase.org>
2024-04-29 16:25:25 -0700

    updated vv for proteomics

--- scripts/split_model.py
+++ scripts/split_model.py
@@ -1,3 +1,4 @@
+#!usr/bin/env python
 """ 
 Split data model by "Module"
 
--- scripts/update_data_dictionary.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# After changes to the data model, this script reflects them in the data dictionary site
-
-python data-dictionary/processes/main_workflow.py
