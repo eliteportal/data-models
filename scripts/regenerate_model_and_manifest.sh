b1d94e65ef7b2d9fd359a8dd1d08b9cc58e1ad81
Nicholas Lee <nicholas.lee@sagebase.org>
2024-04-30 16:45:26 -0700

    fixed spelling errors in DM

--- scripts/regenerate_model_and_manifest.sh
+++ scripts/regenerate_model_and_manifest.sh
@@ -9,17 +9,17 @@ schematic schema convert EL.data.model.csv \
   --output_jsonld EL.data.model.jsonld
 date "+%H:%M:%S   %d/%m/%y"
 
-# for MANIFEST in 'Proteomics'; do
-#   echo -- $MANIFEST
+for MANIFEST in 'Proteomics'; do
+  echo -- $MANIFEST
 
-#   RESULTS=$(
-#     schematic manifest --config ./config.yml \
-#       get -dt $MANIFEST \
-#       -s -oxlsx EL.Manifest.$MANIFEST.xlsx
-#   )
+  RESULTS=$(
+    schematic manifest --config ./config.yml \
+      get -dt $MANIFEST \
+      -s -oxlsx "EL.Manifest.$MANIFEST.xlsx"
+  )
 
-#   echo $RESULTS
-#   NOW=$(date -f "%d/%m/%y +%H:%M:%S")
-#   echo $NOW $MANIFEST ": " $RESULTS >>./data/manifest_generation_results.txt
-#   echo -----------------------------
-# done
+  echo $RESULTS
+  NOW=$(date "%d/%m/%y +%H:%M:%S")
+  echo $NOW $MANIFEST ": " $RESULTS >>./data/manifest_generation_results.txt
+  echo -----------------------------
+done
