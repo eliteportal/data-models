d4d6575494c30fde0464e61f2eee75fd463cc68d
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2024-06-11 22:22:52 +0000

    explain script function

--- data_model_creation/update_data_model.sh
+++ data_model_creation/update_data_model.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-python data_model_creation/join_data_model.py
+python data_model_creation/join_data_model.py # joins the module csvs into full csv data model and converts to json-ld
 
 python data_model_creation/test_generate_manifests.py # will test all manifests are generated properly and then update the dca-template-config.json
 
