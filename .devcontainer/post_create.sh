cdc8ceffca31db0b15016022666217bf202e175e
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2024-08-02 20:25:27 +0000

    update codespace setup

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -5,3 +5,6 @@ poetry install
 
 # open virtual environment with poetry shell
 poetry shell
+
+# write schematic gsheets creds to file for schematic to use
+echo $SCHEMATIC_SERVICE_ACCOUNT_CREDS > ./schematic_service_account_creds.json
